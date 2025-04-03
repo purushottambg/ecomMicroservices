95a2e9dc3a65e18260d6d90c4429fdf1077413af
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-04-04 00:17:15 +0530

    Controller added with working

--- inventory-service/inventory-service/src/main/java/com/inventory_service/controller/ProductController.java
+++ inventory-service/inventory-service/src/main/java/com/inventory_service/controller/ProductController.java
@@ -1,4 +1,20 @@
 package com.inventory_service.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class ProductController {
+
+    @GetMapping("/products")
+    public String showProducts(){
+        return "Products";
+    }
+
+    @GetMapping("/products/{productID}")
+    public String showProductByID(@PathVariable Long productID){
+        return "Products By ID "+productID;
+    }
+
+
 }
