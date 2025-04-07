4afffa6348e0617af82b53fff6725631c3063809
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-04-07 13:26:56 +0530

    Built the DTOs for order placement

--- order-service/order-service/src/main/java/com/order_service/dto/OrderDTO.java
+++
@@ -1,4 +0,0 @@
-package com.order_service.dto;
-
-public class OrderDTO {
-}
--- order-service/order-service/src/main/java/com/order_service/repository/OrderRepository.java
+++ order-service/order-service/src/main/java/com/order_service/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.order_service.repository;
 
-public class OrderRepository {
+import com.order_service.entity.OrdersEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<OrdersEntity, Long> {
 }
