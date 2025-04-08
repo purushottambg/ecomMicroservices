072b43c419f57feaa885254205638bcde9e2c901
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-04-08 05:44:18 +0530

    Discovery service is added for smooth communication among the services

---
+++ discovery-service/src/main/java/com/discovery_service/DiscoveryOfAnServicesApplication.java
@@ -0,0 +1,13 @@
+package com.discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryOfAnServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryOfAnServicesApplication.class, args);
+	}
+
+}
