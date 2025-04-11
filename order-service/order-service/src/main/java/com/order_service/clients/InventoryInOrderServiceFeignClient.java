330ce4500b51a29ddd63541bb51ef120896e00b4
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-04-11 12:04:51 +0530

    Starting with the build of purchase APIs

--- order-service/order-service/src/main/java/com/order_service/clients/InventoryInOrderServiceFeignClient.java
+++ order-service/order-service/src/main/java/com/order_service/clients/InventoryInOrderServiceFeignClient.java
@@ -4,6 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/*
+Mention the service details for which this client is.
+if you want to have a client that will interact with inventory service
+then mention inventory-service name at the beginning
+also add api type followed by return type and path
+ */
+
 @FeignClient(value = "inventory-service" )
 public interface InventoryInOrderServiceFeignClient {
     @GetMapping("/inventory/greet")
