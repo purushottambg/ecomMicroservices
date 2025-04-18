9c6e87a9062790c89a2781d020b8b493c353ac72
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-04-18 13:58:16 +0530

    Challenges

--- order-service/order-service/src/main/java/com/order_service/entity/OrderStatusENum.java
+++
@@ -1,5 +0,0 @@
-package com.order_service.entity;
-
-public enum OrderStatusENum {
-    PENDING, COMPLETE, CANCELLED, CONFIRMED, INPROGRESS
-}
--- order-service/order-service/src/main/java/com/order_service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.order_service.repository;
-
-import com.order_service.entity.OrdersEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrdersEntity, Long> {
-}
