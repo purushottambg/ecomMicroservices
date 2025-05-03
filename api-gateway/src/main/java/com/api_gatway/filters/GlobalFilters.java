a429237de762cf00aecfa9aebe959cefe45c1479
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-03 10:35:54 +0530

    Added the route specific filter also, create filter class, extend Abstract extend AbstractGatewayFilterFactory create config class of your own and tell spring boot to use that one only create config class create constructor and pass instance .class to the super class override the apply method and do desired things there

--- api-gateway/src/main/java/com/api_gatway/filters/GlobalFilters.java
+++ api-gateway/src/main/java/com/api_gatway/filters/GlobalFilters.java
@@ -1,19 +1,34 @@
-//package com.api_gatway.filters;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-//import org.springframework.cloud.gateway.filter.GlobalFilter;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.server.ServerWebExchange;
-//import reactor.core.publisher.Mono;
-//
-//@Component
-//@Slf4j
-//public class GlobalFilters implements GlobalFilter {
-//
-//    @Override
-//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-//        log.warn("Logging from global {}", exchange.getRequest().getURI());
-//        return null;
-//    }
-//}
+package com.api_gatway.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalFilters implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        log.info("Logging filter pre:{}", exchange.getResponse().getStatusCode());
+
+        return chain.filter(exchange).then(Mono.fromRunnable(
+                ()->{
+                    log.info("Logging Filter Post:{}",exchange.getResponse().getStatusCode());
+                }
+                ));
+    }
+
+    @Override
+    public int getOrder(){
+        return 5;
+    }
+
+
+}
---
+++ api-gateway/src/main/java/com/api_gatway/filters/LoggingOrdersFilter.java
@@ -0,0 +1,29 @@
+package com.api_gatway.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LoggingOrdersFilter extends AbstractGatewayFilterFactory<LoggingOrdersFilter.Config> {
+
+    public LoggingOrdersFilter( ){
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+
+        return((exchange, chain) -> {
+            log.info("Route Specific Filters: {} ", exchange.getRequest().getId());
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config{
+
+    }
+
+}
