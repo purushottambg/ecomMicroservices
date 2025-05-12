e29d41b57bebbd7e03a55667bdbc2d4dcbf7dd2a
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-12 18:06:12 +0530

    On main branch

--- api-gateway/src/main/java/com/api_gatway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/api_gatway/ApiGatewayApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-/*This is the entry point for all the services, each request must go through this service, and we can perform many common operations
+/**
+ *This is the entry point for all the services, each request must go through this service, and we can perform many common operations
 like central authorities.
 */
 @SpringBootApplication
