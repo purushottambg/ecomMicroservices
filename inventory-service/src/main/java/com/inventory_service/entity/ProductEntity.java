16561722e06fa0c475be1d5e898f87ef9779a2e1
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-12 18:56:22 +0530

    did the DB modifications and also added two items successfully

--- inventory-service/src/main/java/com/inventory_service/entity/ProductEntity.java
+++ inventory-service/src/main/java/com/inventory_service/entity/ProductEntity.java
@@ -14,7 +14,7 @@ import lombok.*;
 public class ProductEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO )
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long product_id;
 
     private String name;
