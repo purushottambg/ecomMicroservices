06069ad2fd04757a9d174da2c060f171057f1cba
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-13 21:21:12 +0530

    Setting up database file and properties file as well

---
+++ customer-service/customer_service_sql_file.sql
@@ -0,0 +1,3 @@
+create database ms_customer_service;
+use ms_customer_service;
+show tables;
--- inventory-service/inventory_service_sql_file.sql
+++ inventory-service/inventory_service_sql_file.sql
@@ -3,3 +3,7 @@ use ms_inventoryservice;
 show tables;
 
 select * from products;
+
+ALTER TABLE products MODIFY COLUMN product_id BIGINT NOT NULL AUTO_INCREMENT;
+
+commit;
