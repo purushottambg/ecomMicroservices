94e42211c0b55076eaa4e55bfb1d097cc35e300f
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-23 23:42:59 +0530

    Customer DTO has been created

---
+++ customer-service/src/main/java/com/customer_service/controller/CustomerController.java
@@ -0,0 +1,18 @@
+package com.customer_service.controller;
+
+import com.customer_service.dtos.CustomerDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "customer")
+public class CustomerController {
+
+    @PostMapping
+    public String createCustomer(@RequestBody CustomerDTO customerDTO){
+
+        return "Customer Has been successfully Created!";
+    }
+}
---
+++ customer-service/src/main/java/com/customer_service/dtos/CustomerDTO.java
@@ -0,0 +1,10 @@
+package com.customer_service.dtos;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CustomerDTO {
+    private String name;
+    private Long phone;
+    private String address;
+}
