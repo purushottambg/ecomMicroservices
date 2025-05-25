d78b3a18167a9367d3e95e80b8010d1f5c3ddfe3
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-25 21:11:31 +0530

    Able to create users in the table

--- customer-service/src/main/java/com/customer_service/controller/CustomerController.java
+++ customer-service/src/main/java/com/customer_service/controller/CustomerController.java
@@ -1,11 +1,11 @@
 package com.customer_service.controller;
 
-import com.customer_service.dtos.CustomerDTO;
 import com.customer_service.dtos.SignUpDTO;
 import com.customer_service.entity.CustomerEntity;
 import com.customer_service.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,19 +17,14 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CustomerController {
 
+    @Autowired
     private ModelMapper modelMapper;
+    @Autowired
     private CustomerService customerService;
 
     @PostMapping("/sign-up")
     public ResponseEntity<?> createCustomer(@RequestBody SignUpDTO customerDTO){
-        CustomerEntity customerEntityToBeSaved = modelMapper.map(customerDTO, CustomerEntity.class);
         Optional<CustomerEntity> savedCustomer = customerService.createUser(customerDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCustomer);
     }
-
-//    @GetMapping("/login")
-//    public ResponseEntity<?> logInCustomer(@RequestBody CustomerDTO customerDTO){
-//         Optional<CustomerEntity> foundCustomer = customerService.validateLoginRequest(customerDTO);
-//         return ResponseEntity.status(HttpStatus.FOUND).body(foundCustomer);
-//    }
 }
--- customer-service/src/main/java/com/customer_service/service/CustomerService.java
+++ customer-service/src/main/java/com/customer_service/service/CustomerService.java
@@ -1,6 +1,6 @@
 package com.customer_service.service;
 
-import com.customer_service.dtos.CustomerDTO;
+
 import com.customer_service.dtos.SignUpDTO;
 import com.customer_service.entity.CustomerEntity;
 import com.customer_service.repository.CustomerRepo;
@@ -12,22 +12,18 @@ import java.util.Optional;
 
 
 @Service
-@RequiredArgsConstructor
-public class CustomerService {
 
-    private CustomerDTO customerDTO;
+public class CustomerService {
     private CustomerRepo customerRepo;
-    private CustomerEntity customerEntity;
     private ModelMapper modelMapper;
+    public CustomerService(CustomerRepo customerRepo, ModelMapper modelMapper) {
+        this.customerRepo = customerRepo;
+        this.modelMapper = modelMapper;
+    }
 
     public Optional<CustomerEntity> createUser(SignUpDTO customerDTO) {
         CustomerEntity customerEntityToBeSaved = modelMapper.map(customerDTO, CustomerEntity.class);
         return Optional.ofNullable(customerRepo.save(customerEntityToBeSaved));
     }
 
-
-//    public Optional<CustomerEntity> validateLoginRequest(CustomerDTO customerDTO) {
-//        Optional<CustomerEntity> savedEntity = customerRepo.findByID(customerDTO.getId());
-//        return savedEntity;
-//    }
 }
