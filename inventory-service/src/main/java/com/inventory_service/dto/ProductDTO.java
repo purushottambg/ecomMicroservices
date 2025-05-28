ff886524c28605bb6ac135f52dcecf67cf755f6f
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-28 23:57:41 +0530

    The Issue with the DTO that DTO should've no parameter contructor has been solved with @NoArgsConstructor

--- inventory-service/src/main/java/com/inventory_service/dto/ProductDTO.java
+++ inventory-service/src/main/java/com/inventory_service/dto/ProductDTO.java
@@ -1,14 +1,12 @@
 package com.inventory_service.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductDTO {
 
     private Long product_id;
