17f9d090829c9d101f93256914060a3e64bd6f8f
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-05-28 23:52:31 +0530

    inventory service had issue with non private constructor

--- inventory-service/src/main/java/com/inventory_service/dto/ProductDTO.java
+++ inventory-service/src/main/java/com/inventory_service/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package com.inventory_service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
 public class ProductDTO {
 
     private Long product_id;
